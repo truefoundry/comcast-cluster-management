f86fc045b7f039f138a7e6464ef64ed105d4b9d3
vivek-tfy <vivek.ks@truefoundry.com>
2026-03-24 15:57:29 +0530

    fix: eslint ignore migration file

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs'],
+    ignores: ['eslint.config.mjs', 'src/database/**/*.js', 'src/database/**/*.cjs'],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
